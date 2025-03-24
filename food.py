416976869f433e2337b0a1b345763d05dc07247d
sthagaurav <gauravdlg@gmail.com>
2025-03-24 10:31:32 +0545

    Management system

---
+++ food.py
@@ -0,0 +1,11 @@
+# Defining the function cuisine and making lists.
+def cuisines (
+    Japanese = ["Ramen", "Termpura", "Okonimiyaki","Onigiri"], 
+    Italian  = ["Lasagna", "Risotto", "Vitello Tonnato", "Spaghetti alla carbonara"], 
+    French   = ["Ratatouille", "Coq au vin", "Duck confit", "Salade Niçoise", "Crêpes"], 
+    Thai     = ["Pad thai", "Som Tam", "Khao soi", "Tom kha gai", "Kaeng Lueang"], 
+    Mexiacan = ["Flan", "Tostadas de Ceviche", "Tacos al pastor", "Enchiladas", "Crème caramel", "Burritos"],
+    Chinese  = ["Kunf pao chicken", "Peking duck", "Char siu", "Ma Po Tofu", "Dumplings", "Xiaolongbao"],
+    Korean   = ["Japchae", "Tteokbokki", "Bulgoki", "Kimchi Fried Rice", "Bibimbap", "Kimchi", "Mandu"], 
+    Nepali   = ["Dal Bhat", "Momo", "Bara", "Dhindho", "Samay Baji", "Choila", "Sel roti"])
+    
